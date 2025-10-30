5162a01aea3a7c619ac5e0801d88f2b65dcc2222
Deepak Rana <149474424+deepxkrana@users.noreply.github.com>
2025-10-30 15:11:50 +0530

    update

---
+++ INT219/Tailwind/src/index.js
@@ -0,0 +1,8 @@
+console.log("hello shubham dalle")
+// alert("How are you!!!!")
+function greet(){
+    alert("Hello World ke Bhai Behen");
+}
+// let name="Deepak"
+// let age=21;
+// alert("name: "+name);
